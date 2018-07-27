a0223de8d13a97e4128c34d15eef7c536349ecc1
andrzejmara <andrzejmara@gmail.com>
2018-07-27 13:02:32 +0200

    Database Scheme and Added Person Controller and Home Controller

--- PhoneBook/Models/PersonModel.cs
+++ PhoneBook/Models/PersonModel.cs
@@ -12,14 +12,15 @@ namespace PhoneBook.Models
         public int ID { get; set; }
 
         [Required]
-        [MaxLength(20)]
+        [MaxLength(32)]
         public string FirstName { get; set; }
 
         [Required]
-        [MaxLength(20)]
+        [MaxLength(64)]
         public string LastName { get; set; }
 
         [Required]
+		[MaxLength(25)]
         public string Phone { get; set; }
 
         [EmailAddress]
---
+++ WarsztatySqlQuery.sql
@@ -0,0 +1,10 @@
+CREATE TABLE People(
+ID Int Identity(1,1) NOT NULL PRIMARY KEY ,
+FirstName nvarchar(32) NOT NULL,
+LastName nvarchar(64) NOT NULL,
+Phone nvarchar(25) NOT NULL,
+Email nvarchar(256) NOT NULL,
+Created DateTime,
+Updated DateTime
+); 
+
